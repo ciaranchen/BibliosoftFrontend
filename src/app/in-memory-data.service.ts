0ba5c372ba1a2ff1068d1a29b26caf859b24726e
416013996@qq.com <37134516+zcyzuibang@users.noreply.github.com>
2018-10-09 07:47:46 +0800

    modified:   src/app/in-memory-data.service.ts 	modified:   src/app/librarian-search-book/librarian-search-book.component.html 	modified:   src/app/librarian-search-book/librarian-search-book.component.ts

--- src/app/in-memory-data.service.ts
+++ src/app/in-memory-data.service.ts
@@ -4,25 +4,25 @@ export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     const books = [
       {
-        title: '考研数学复习全书',
+        Title: '考研数学复习全书',
         ISBN: '9787515020303',
-        author: '李永乐',
-        date: '2017-12',
-        summary: '数学辅导书'
+        Author: '李永乐',
+        Date: '2017-12',
+        Summary: '数学辅导书'
       },
       {
-        title: '百年孤独',
+        Title: '百年孤独',
         ISBN: '9787544253994',
-        author: '[哥伦比亚]] 加西亚 · 马尔克斯 ',
-        date: '2011-6',
-        summary: '《百年孤独》是魔幻现实主义文学的代表作，描写了布恩迪亚家族七代人的传奇故事，以及加勒比海沿岸小镇马孔多的百年兴衰，反映了拉丁美洲一个世纪以来风云变幻的历史.'
+        Author: '[哥伦比亚]] 加西亚 · 马尔克斯 ',
+        Date: '2011-6',
+        Summary: '《百年孤独》是魔幻现实主义文学的代表作，描写了布恩迪亚家族七代人的传奇故事，以及加勒比海沿岸小镇马孔多的百年兴衰，反映了拉丁美洲一个世纪以来风云变幻的历史.'
       },
       {
         title: 'Python Cookbook',
         ISBN: '9780596007973',
-        author: 'Alex Martelli / Anna Ravenscroft / David Ascher ',
-        date: '2005-3',
-        summary: 'e Python Cookbook, 2nd Edition offers a wealth of useful code for all Python programmers.'
+        Author: 'Alex Martelli / Anna Ravenscroft / David Ascher ',
+        Date: '2005-3',
+        Summary: 'e Python Cookbook, 2nd Edition offers a wealth of useful code for all Python programmers.'
       },
     ];
     return { books };
--- src/app/librarian-search-book/librarian-search-book.component.ts
+++ src/app/librarian-search-book/librarian-search-book.component.ts
@@ -20,7 +20,7 @@ export class LibrarianSearchBookComponent implements OnInit {
   bookbyISBN:Book;
   UserName = 'WYJ';
   search_text: string;
-  search_type: string;
+  search_type: string = 'Title';
 
   constructor(private bookService: BookService) { }
 
@@ -42,17 +42,21 @@ export class LibrarianSearchBookComponent implements OnInit {
   onSelect(b: Book): void {
     this.selectedbook = b;
   }
-  onClick(type: string): void {
-    console.log('start search: "' + this.search_text + '" by ' + type);
+  onClick(): void {
+    // this.search_type = type;
+    console.log('start search: "' + this.search_text + '" by ' + this.search_type);
 
 // 
-    if(type=="ISBN"){;
+    if(this.search_type=="ISBN"){;
       this.getBook(this.search_text);
     }
 
 
     else 
-    this.getBooks(this.search_text,type);
+    this.getBooks(this.search_text,this.search_type);
+  }
+  updateType(type: string){
+    this.search_type = type;
   }
   closeAlert(): void {
     this.selectedbook = null;
