5c6edfbc5c2a958bf43c86fad2b3d82a115aa442
18292821405 <760218327@qq.com>
2018-10-09 09:07:10 +0800

    update admin-login

--- src/app/admin-login/admin-login.component.ts
+++ src/app/admin-login/admin-login.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { apiServer } from '../api-server';
 
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
   styleUrls: ['./admin-login.component.css']
 })
-export class AdminLoginComponent implements OnInit {
+export class AdminLoginComponent {
+  user: string;
+  pass: string;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  ngOnInit() {
+  admin_login(user?: string, pass?: string) {
+    const body = new URLSearchParams();
+    body.set('user', user ? user : this.user);
+    body.set('pass', pass ? pass : this.pass);
+    console.log(body.toString());
+    this.http.post(`${apiServer.get_url()}/login`, body.toString(), {
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    }).subscribe(
+      val => {
+        console.log('login success!');
+        location.href = '/admin/home';
+      },
+      err => {
+        console.log('login failed');
+      },
+      () => {}
+    );
   }
-
 }
